7583e8cc1d17c123a1a5d920c5532fedda065f37
Brian Melton <brianmel@microsoft.com>
2015-08-14 00:59:52 -0700

    Bugfix for primitive wrapper types & null values

--- stanley/src/main/java/com/brianjmelton/stanley/ProxyGenerator.java
+++ stanley/src/main/java/com/brianjmelton/stanley/ProxyGenerator.java
@@ -108,7 +108,12 @@ public class ProxyGenerator {
                 return methodInfo.type.get(sharedPreferences, methodInfo);
             } else {
                 // setter
-                Class<?> paramType = method.getParameterTypes()[0];
+                Class<?> paramType;
+                if (null == args[0]) {
+                    paramType = method.getParameterTypes()[0];
+                } else {
+                    paramType = args[0].getClass();
+                }
                 persistenceDelegates.get(paramType)
                         .persist(sharedPreferences.edit(), methodInfo.key, args[0]);
             }
